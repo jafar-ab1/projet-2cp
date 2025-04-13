9f90f6994ee74259385aa2c42d3420cf8599daa9
mohaal22 <amrimohamed828@gmail.com>
2025-04-13 20:20:11 +0100

    message

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -59,5 +59,5 @@ app.use((req, res) => {
 
 
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server is running at  : http://localhost:${port}`);
 });
