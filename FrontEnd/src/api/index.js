a519bddf027a8e02f5fd41e802f42710ddf0b824
jafar._.ab <jabu62164@gmail.com>
2025-04-15 04:21:25 +0200

    sign-in-up redirecting to homepage

--- FrontEnd/src/api/index.js
+++ FrontEnd/src/api/index.js
@@ -1,13 +1,19 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3000"
+  baseURL: "http://localhost:3000",
 });
 
-export const loginUser = async(loginUserData) => {
-    return api.post("/auth/login", loginUserData);
-}
+export const loginUser = async (loginUserData) => {
+  try {
+    const response = await api.post("/auth/login", loginUserData);
+    return response.data; 
+  } catch (error) {
+    console.error("Login failed:", error);
+    throw error;
+  }
+};
 
-export const registerUser = async(registerUserData) => {
-    return api.post("/auth/register", registerUserData);
-}
+export const registerUser = async (registerUserData) => {
+  return api.post("/auth/register", registerUserData).then(res => res.data); 
+};
