b3fccc060662167f2e6b16ac9de9148366066630
Bouhadda Mohammed Djaoued <bhdmeddjaoued@gmail.com>
2025-04-18 02:25:48 +0300

    navbar edited on big screens

--- FrontEnd/src/components/Accomodation/sections/Branches/Branch/Branch.jsx
+++ FrontEnd/src/components/Accomodation/sections/Branches/Branch/Branch.jsx
@@ -29,7 +29,7 @@ function NavigationDiv() {
         </Link>
       </button>
       <button type="button">
-        <Link to="/Algiers">
+        <Link to="/">
           learn more
           <svg
             xmlns="http://www.w3.org/2000/svg"
--- FrontEnd/src/components/shared/static/NavBar/NavBar.jsx
+++ FrontEnd/src/components/shared/static/NavBar/NavBar.jsx
@@ -1,35 +1,87 @@
-import styles from "./NavBar.module.css"
-import { Link } from "react-router-dom"
-import useAuth from "../../../../hooks/auth/useAuth";
+import styles from './NavBar.module.css';
+import { Link } from 'react-router-dom';
+import useAuth from '../../../../hooks/auth/useAuth';
 
 export default function NavBar() {
-    const { accessToken, actions: {logout}}=useAuth();
-    return(
-        <div className={styles.navbar}>
-            <div className={styles["left-section"]}>
-                <p>HOTEL<br />NAME</p>
-            </div>
-            <ul className={styles["middle-section"]}>
-                <li><Link to="/accomodation">Accommodation</Link></li>
-                <li><Link to="/occasions">Occasions</Link></li>
-                <li><Link to="/events">Events</Link></li>
-                <li><Link to="/wellness">Wellness</Link></li>
-                <li><Link to="/dining">Dining</Link></li>
-            </ul>
-            <div className={styles["right-section"]}>
-                {accessToken ? (
-                <button className={styles["sign-in"]} onClick ={logout}>logout</button>
-                ):(
-                    <Link to ="/sign-in">
-                        <button className={styles["sign-in"]}>Sign In</button>
-                    </Link>
-                )}
-            </div>
-            <button className={styles.button}>
-                <svg width="16" height="12" viewBox="0 0 16 12" fill="none" xmlns="http://www.w3.org/2000/svg" className={styles.burger}>
-                    <path d="M0 0H16V2H0V0ZM0 5H16V7H0V5ZM0 10H16V12H0V10Z" fill="white"/>
-                </svg>
-            </button>
+  const {
+    accessToken,
+    actions: { logout },
+  } = useAuth();
+
+  return (
+    <div className={styles.navbar}>
+      <div className={styles['left-section']}>
+        <p>
+          HOTEL
+          <br />
+          NAME
+        </p>
       </div>
-    )
+      <ul className={styles['middle-section']}>
+        <li>
+          <Link to="/accomodation">Accommodation</Link>
+          <div className={styles['extra-links-container']}>
+            <div>
+              <Link to="/algiers">algiers</Link>
+            </div>
+            <div>
+              <Link to="/oran">oran</Link>
+            </div>
+            <div>
+              <Link to="/annaba">annaba</Link>
+            </div>
+          </div>
+        </li>
+        <li>
+          <Link to="/occasions">Occasions</Link>
+        </li>
+        <li>
+          <Link to="/events">Events</Link>
+        </li>
+        <li>
+          <Link to="/wellness">Wellness</Link>
+        </li>
+        <li>
+          <Link to="/dining">Dining</Link>
+          <div className={styles['extra-links-container']}>
+            <div>
+              <Link to="#">the gourmet spot</Link>
+            </div>
+            <div>
+              <Link to="#">the golden plate</Link>
+            </div>
+            <div>
+              <Link to="#">the coffee nook</Link>
+            </div>
+          </div>
+        </li>
+      </ul>
+      <div className={styles['right-section']}>
+        {accessToken ? (
+          <button className={styles['sign-in']} onClick={logout}>
+            logout
+          </button>
+        ) : (
+          <Link to="/sign-in">
+            <button className={styles['sign-in']}>Sign In</button>
+          </Link>
+        )}
+      </div>
+      <button className={styles.button}>
+        <svg
+          width="16"
+          height="12"
+          viewBox="0 0 16 12"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className={styles.burger}
+        >
+          <path
+            d="M0 0H16V2H0V0ZM0 5H16V7H0V5ZM0 10H16V12H0V10Z"
+            fill="white"
+          />
+        </svg>
+      </button>
+    </div>
+  );
 }
