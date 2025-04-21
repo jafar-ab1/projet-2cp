0b3bc9c906f21d117434b4ef67c7fd1df7e4b574
jafar-ab1 <jabu62164@gmail.com>
2025-04-21 11:04:33 +0300

    integrating Image component into the wellness page

--- FrontEnd/src/components/Wellness/WellnessThings/WellnessThing/WellnessThing.jsx
+++ FrontEnd/src/components/Wellness/WellnessThings/WellnessThing/WellnessThing.jsx
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom';
 import styles from './WellnessThing.module.css';
 
 import FlexContainer from '../../../shared/static/basic/FlexContainer/FlexContainer';
+import Image from '../../../shared/static/basic/Image/Image';
 
 // interface WellnessThingProps {
 //    image: any;
@@ -13,12 +14,8 @@ import FlexContainer from '../../../shared/static/basic/FlexContainer/FlexContai
 export default function WellnessThing({ image, title, description, reverse }) {
   return (
     <FlexContainer reverse={reverse} className={styles.container}>
-      <div className={styles['image-container']}>
-        <div
-          className={styles['image-inner-container']}
-          style={{ backgroundImage: `url(${image})` }}
-        ></div>
-      </div>
+      <Image image={image} reverse={reverse} />
+
       <div className={styles['inner-container']}>
         <div className={styles['info-container']}>
           <h1>{title}</h1>
