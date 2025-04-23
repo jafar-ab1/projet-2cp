4506ec4a12ce7acf3136f1dcdcc16e206dcf923c
jafar-ab1 <jabu62164@gmail.com>
2025-04-23 11:24:44 +0300

    NavBar component Links sub comp code changed for better scalability, load for images fixed by adding animations

--- FrontEnd/src/components/shared/static/NavBar/Links/Links.jsx
+++ FrontEnd/src/components/shared/static/NavBar/Links/Links.jsx
@@ -1,3 +1,4 @@
+import { menuLinks } from '../../../../../constants';
 import styles from './Links.module.css';
 
 import { Link } from 'react-router-dom';
@@ -5,43 +6,18 @@ import { Link } from 'react-router-dom';
 export default function Links() {
   return (
     <ul className={styles.container}>
-      <li>
-        <Link to="/accomodation">Accommodation</Link>
-        <div className={styles['extra-links-container']}>
-          <div>
-            <Link to="/algiers">algiers</Link>
-          </div>
-          <div>
-            <Link to="/oran">oran</Link>
-          </div>
-          <div>
-            <Link to="/annaba">annaba</Link>
-          </div>
-        </div>
-      </li>
-      <li>
-        <Link to="/occasions">Occasions</Link>
-      </li>
-      <li>
-        <Link to="/events">Events</Link>
-      </li>
-      <li>
-        <Link to="/wellness">Wellness</Link>
-      </li>
-      <li>
-        <Link to="/dining">Dining</Link>
-        <div className={styles['extra-links-container']}>
-          <div>
-            <Link to="#">the gourmet spot</Link>
-          </div>
-          <div>
-            <Link to="#">the golden plate</Link>
-          </div>
-          <div>
-            <Link to="#">the coffee nook</Link>
-          </div>
-        </div>
-      </li>
+      {menuLinks.map(({ name, to, subLinks }) => (
+        <li>
+          <Link to={to}>{name}</Link>
+          {subLinks.length !== 0 && (
+            <div className={styles['extra-links-container']}>
+              {subLinks.map(({ name, to }) => (
+                <Link to={to}>{name}</Link>
+              ))}
+            </div>
+          )}
+        </li>
+      ))}
     </ul>
   );
 }
