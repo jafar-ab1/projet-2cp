71d3738034d17ea027e8aff2c09ab363dcd3627a
younes <amriyounes890@gmail.com>
2025-05-21 00:06:26 +0100

    addGuest roomNumber array

--- BackEnd/server/validation/guestValidation.js
+++ BackEnd/server/validation/guestValidation.js
@@ -5,9 +5,18 @@ const addGuest = Joi.object({
               "string.email": "email must be of type Email",
               "any.required": "email is required"
           }),
-    roomNumber: Joi.string().required().alphanum().messages({
-            'any.required': 'Le numéro de chambre est obligatoire'
-            })
+    roomNumber: Joi.array().items(
+    Joi.string().required()
+      .messages({
+        'any.required': 'Le numéro de chambre est obligatoire'
+      })
+  ).min(1).max(20).required()
+    .messages({
+      'array.base': 'roomNumbers doit être un tableau',
+      'array.min': 'Au moins une chambre doit être fournie',
+      'array.max': 'Maximum 20 chambres par requête',
+      'any.required': 'roomNumber est obligatoire'
+    })
 })
 
 module.exports = {addGuest};
